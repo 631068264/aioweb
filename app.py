42b751596e3d72b54a4adbfb176dfbed2248162d
wyx <wyx@wuyuxideMacBook-Pro.local>
2016-07-31 21:07:34 +0800

    add aiohttp_route_decorator方便开发

--- app.py
+++ app.py
@@ -5,45 +5,12 @@
 @time = 16/7/29 12:08
 @annotation = '' 
 """
-import asyncio
 
+import views
 from aiohttp import web
 
-
-# TODO:对外接口
-async def index(request):
-    body = b'Hello world'
-    return web.Response(body='<h1>是打发斯蒂芬, %s!</h1>'.encode('utf-8'))
-
-
-async def hello(request):
-    await asyncio.sleep(0.5)
-    h = request.GET["sdf"]
-    text = '<h1>是打发斯蒂芬, %s!</h1>' % request.match_info['name']
-    return web.Response(text=text)
-
-
-async def df(request):
-    await asyncio.sleep(0.5)
-    # ll = await request.post.get["asdfad"]
-    sdf = await request.post()
-    sf = sdf.get("asdfad")
-    text = '<h1>hello, %s!</h1>' % request.match_info['name']
-    return web.Response(body=text.encode('utf-8'))
-
-
-async def jsonpost(request):
-    await asyncio.sleep(0.5)
-    # ll = await request.post.get["asdfad"]
-    sdf = await request.json()
-    sdf = sdf["data"]
-
-    return web.json_response({"fadfadf": 1})
-
-
 app = web.Application()
-app.router.add_route('GET', '/', index)
-app.router.add_route('GET', '/{name}', hello)
-app.router.add_route("POST", "/hello", jsonpost)
-# app.router.add_route("POST", "/hello", df)
+for name in views.__all__:
+    module = __import__('views.%s' % name, fromlist=[name])
+    module.route.add_to_router(app.router)
 web.run_app(app, port=8080, shutdown_timeout=15)
---
+++ views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author = 'wyx'
+@time = 16/7/31 18:46
+@annotation = '' 
+"""
+__all__ = ['test']
