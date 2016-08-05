fece821116d17f9b330dbda099052417c24b43c7
wyx <wyx@wuyuxideMacBook-Pro.local>
2016-08-05 23:18:01 +0800

    优化fcm工具类

--- util/fcm/fcm.py
+++ util/fcm/fcm.py
@@ -59,58 +59,31 @@ class FCMNotification(FCMAPI):
                     FCM_CONFIG['TIME_TO_LIVE'][1]:
                 return False, "InvalidTtl"
 
-        if len(registration_ids) > FCM_CONFIG['MAX_REGIDS']:
-            payloads = []
-            for regids in self.get_regids_chunks(registration_ids):
-                payloads.append(self.parse_payload(task_id=task_id,
-                                                   registration_ids=regids,
-                                                   message_body=message_body,
-                                                   message_title=message_title,
-                                                   data_message=data_message,
-                                                   low_priority=low_priority,
-                                                   collapse_key=collapse_key,
-                                                   delay_while_idle=delay_while_idle,
-                                                   time_to_live=time_to_live,
-                                                   topic_name=topic_name,
-                                                   condition=condition,
-                                                   click_action=click_action,
-                                                   tag=tag,
-                                                   dry_run=DEBUG,
-                                                   color=color,
-                                                   message_icon=message_icon,
-                                                   sound=sound,
-                                                   body_loc_key=body_loc_key,
-                                                   body_loc_args=body_loc_args,
-                                                   title_loc_key=title_loc_key,
-                                                   title_loc_args=title_loc_args,
-                                                   restricted_package_name=restricted_package_name
-                                                   ))
-        else:
-            payload = self.parse_payload(task_id=task_id,
-                                         registration_ids=registration_ids,
-                                         message_body=message_body,
-                                         message_title=message_title,
-                                         data_message=data_message,
-                                         low_priority=low_priority,
-                                         collapse_key=collapse_key,
-                                         delay_while_idle=delay_while_idle,
-                                         time_to_live=time_to_live,
-                                         topic_name=topic_name,
-                                         condition=condition,
-                                         click_action=click_action,
-                                         tag=tag,
-                                         dry_run=DEBUG,
-                                         color=color,
-                                         message_icon=message_icon,
-                                         sound=sound,
-                                         body_loc_key=body_loc_key,
-                                         body_loc_args=body_loc_args,
-                                         title_loc_key=title_loc_key,
-                                         title_loc_args=title_loc_args,
-                                         restricted_package_name=restricted_package_name
-                                         )
-            payloads = [payload]
-
+        payloads = []
+        for regids in self.get_regids_chunks(registration_ids):
+            payloads.append(self.parse_payload(task_id=task_id,
+                                               registration_ids=regids,
+                                               message_body=message_body,
+                                               message_title=message_title,
+                                               data_message=data_message,
+                                               low_priority=low_priority,
+                                               collapse_key=collapse_key,
+                                               delay_while_idle=delay_while_idle,
+                                               time_to_live=time_to_live,
+                                               topic_name=topic_name,
+                                               condition=condition,
+                                               click_action=click_action,
+                                               tag=tag,
+                                               dry_run=DEBUG,
+                                               color=color,
+                                               message_icon=message_icon,
+                                               sound=sound,
+                                               body_loc_key=body_loc_key,
+                                               body_loc_args=body_loc_args,
+                                               title_loc_key=title_loc_key,
+                                               title_loc_args=title_loc_args,
+                                               restricted_package_name=restricted_package_name
+                                               ))
         tasks = []
         results = []
         for payload in payloads:
